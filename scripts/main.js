e7b523b89f0ff284695751f3b592b4965d722e5b
Ashlee Salinas <ashlee.salinas24@student.cart.org>
2025-01-22 09:32:09 -0800

    all done

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,32 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello World!";
+
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+});
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1")
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent= `Mozialla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+    setUserName();
+});
